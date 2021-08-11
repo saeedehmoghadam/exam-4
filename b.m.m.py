c82c3c083cac617ecb8244c5fc1277cdc37c7013
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-11 16:23:35 +0430

    Add files via upload

---
+++ b.m.m.py
@@ -0,0 +1,32 @@
+import math
+
+x=int (input("type the number1 : "))
+y=int(input("type the number2 : "))
+
+i=0
+
+if x % 2 == 0 or y% 2==0 :
+
+    print(2)
+
+    b =(x,y)/2
+
+elif  (x,y) % 2 != 0:
+
+   
+    for i in range((x,y) % i == 0):
+
+        print(i)
+
+        (x,y/i)
+
+        i=i+2
+        
+
+        if (x,y) > 2 :
+
+            print((x,y))
+ 
+
+
+
